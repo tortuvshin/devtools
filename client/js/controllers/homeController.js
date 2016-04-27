9150fe2c6cf36f84900940f8b75b225a24758ad1
tortuvshin <toroo.byamba@gmail.com>
2016-04-28 02:36:52 +0900

    homecontroller updated

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -1,3 +1,11 @@
 myApp.controller('homeController', ['$scope', function($scope){
-	
+	$scope.slides = [{
+		image: "img/nodejs.png"
+	},
+	{
+		image: "img/nodejs1.png"
+	},
+	{
+		image: "img/nodejs2.png"
+	}]
 }])
