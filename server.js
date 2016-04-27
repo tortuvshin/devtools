fe766a4372cd907a55884928dcd995f8291e579f
tortuvshin <toroo.byamba@gmail.com>
2016-04-27 23:28:51 +0900

    nodejs server created

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//var port = process.env.PORT;
+var port = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'client', 'views'));
+
+app.get('/', function(req, res){
+	res.render('index.ejs');
+})
+
+app.listen(port, function(){
+	console.log('SERVER RUNNING ...' + 8000)
+})
